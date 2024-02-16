9ec31a0089f7bc4a7d903a8543674bc60f50575d
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-16 09:51:10 -0700

    Add #foods,#total_hits, and attrreader to FoodFacade class

---
+++ app/facades/food_facade.rb
@@ -0,0 +1,19 @@
+class FoodFacade
+  attr_reader :q
+
+  def initialize(params)
+    @params = params
+    @q = params[:q]
+  end
+
+  def foods
+    service = FoodService.new.food_search(@q)[:foods]
+    m = service.map do |food_data|
+      Food.new(food_data)
+    end
+  end
+
+  def total_hits
+    service = FoodService.new.food_search(@q)[:totalHits]
+  end
+end
